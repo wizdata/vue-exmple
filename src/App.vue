ae55313fa125d73d029d6a392c845e18be9e25d8
Xu <0670409987@uscis.dhs.gov>
2022-02-23 12:27:19 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import Home from './components/Home.vue'
+import NotFound from './components/NotFound.vue'
+
+const routes = {
+  '': Home,
+  '/': Home,
+  '#/es': Home 
+}
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath]
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
+  }
+}
+</script>
+
+<template>
+  <a href="/#/es">Spanish</a>
+  <component :is="currentView" />
+</template>
---
+++ src/components/languages.vue
@@ -0,0 +1,23 @@
+<script>
+export const lang = {
+    en: {
+      text: 'English',
+      Topic: {
+        value: 'Topic'
+      },
+      Forms: {
+        value: 'Forms'
+      }
+    },
+    es: { 
+      text: 'Spanish',
+      Topic: {
+        value: 'temas'
+      },
+      Forms: {
+        value: 'temas'
+      }
+
+    }
+};
+</script>
